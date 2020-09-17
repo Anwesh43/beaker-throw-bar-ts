85b682c9b5ad66724db03fda3de22f770b7252eb
Anwesh43 <anweshthecool0@gmail.com>
2020-09-17 13:42:12 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { min } from "lodash"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const lines : number = 3 
@@ -13,3 +15,18 @@ const colors : Array<string> = [
     "#FFEB3B"
 ]
 const foreColor : string = "#bdbdbd"
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
