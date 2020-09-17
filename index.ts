c86448e872e2199aa91c78fb5096217c74b7e4f7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-17 14:11:13 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { min } from "lodash"
-
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const lines : number = 3 
@@ -64,5 +62,36 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawThrowBar(context, scale)
+    }
+}
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = foreColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
